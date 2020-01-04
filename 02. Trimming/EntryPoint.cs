874871b3a4f182bf2ebcd78b78577c3f46c4bdf0
Lockeheede <prescott.lawani@gmail.com>
2020-01-04 15:09:41 -0600

    Add project files.

---
+++ 02. Trimming/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    class EntryPoint
+    {
+        static void Main()
+        {
+        string name = "  PJ  ";
+        name = name.TrimStart();
+
+        Console.WriteLine(name);
+        }
+    }
+
