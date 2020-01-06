a5537459d39165e6ba288e5c359d023af8cfcfc3
Lockeheede <prescott.lawani@gmail.com>
2020-01-06 13:38:00 -0600

    Added string splitting

---
+++ 01. Splitting Strings/EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+    class EntryPoint
+    {
+        static void Main()
+        {
+        char[] charactersToSplitBy = {',',' '};
+        string characters = "TheThief TheMuse TheRat TheSage TheTroll TheMoney TheCoder ThePresident " +
+            "TheLame TheHustler TheChain TheMusic";
+        string creatures = "TangTangila, Flaunchy, Hemogoblin, FlyingRat, DangerDog, BlueSnake";
+        
+        string[] splitCharacters = characters.Split(' ');//The split command seperates the information by a character
+        string[] splitCreatures = creatures.Split(charactersToSplitBy, StringSplitOptions.RemoveEmptyEntries);
+       
+
+        foreach (var creature in splitCreatures)
+        {
+            Console.WriteLine(creature);
+        }
+
+        //Remember to install the selenium webdriver and selenium webdriver chromedriver to use the following:
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todvachev.com/sitemap-posttype-post.xml");
+
+        string[] pageSource = driver.PageSource.Split('>');
+
+        foreach (var line in pageSource)
+        {
+            Console.WriteLine(line);
+        }
+        }
+    }
+
